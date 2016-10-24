21997ab080a81f47cfe25b32c2cb9c4530fd72a7
Durendal <durendal@durendals-domain.com>
2016-10-23 21:49:31 -0600

    Removed some redundancies

--- decrypt.py
+++ decrypt.py
@@ -6,15 +6,10 @@ from rc6.helpers import generateKey, deBlocker
 def main():
     cdec() if len(sys.argv) > 1 else dec()
     
-def dec():
-    print "DECRYPTION: "
-
-    key =raw_input("Enter Key(0-16 characters): ")
+def decData(key):
     if len(key) <16:
         key += " "*(16-len(key))
     key = key[:16]
-                         
-    print "UserKey: %s" % key 
     s = generateKey(key)
     f = open("encrypted.txt","r")
     if not f:
@@ -24,6 +19,16 @@ def dec():
         esentence = f.readline()
     cipher,orgi = decrypt(esentence,s)
     sentence = deBlocker(orgi)
+
+    return (cipher, orgi, esentence, sentence)
+
+def dec():
+    print "DECRYPTION: "
+
+    key =raw_input("Enter Key(0-16 characters): ")
+                         
+    print "UserKey: %s" % key 
+    cipher, orgi, esentence, sentence = decData(key)
     print "\nEncrypted String list: ",cipher
     print "Encrypted String: %s" % esentence
     print "Length of Encrypted String: %d" % len(esentence)
@@ -33,28 +38,14 @@ def dec():
     print "Length of Decrypted String: %d" % len(sentence)
     
 def cdec():
-    print
     if(len(sys.argv)) < 2:
         print "Usage: python cenc.py <key> optional(filename)"
         sys.exit(0)
 
     key = sys.argv[1]
-    if len(key) <16:
-        key = key + " "*(16-len(key))
-    key = key[:16]
-                          
-    s = generateKey(key)
-    loc = "encrypted.txt"
-    if len(sys.argv) >=3:
-        loc = sys.argv[2]
-    f = open(loc,"r")
-    if not f:
-        print "Encrypted input not found in %s" % loc
-        sys.exit(0)
-    else:
-        esentence = f.readline()
-    cipher,orgi = decrypt(esentence,s)
-    sentence = deBlocker(orgi)
+    
+    cipher, orgi, esentence, sentence = decData(key)
+
     print "\nDecrypted String list: ",orgi
     print "Decrypted String: %s" % sentence 
     
--- encrypt.py
+++ encrypt.py
@@ -6,24 +6,34 @@ from rc6.helpers import generateKey, deBlocker
 def main():
     cenc() if len(sys.argv) > 1 else enc()
 
-def enc():
-    print "ENCRYPTION: "
-
-    key =raw_input("Enter Key(0-16 characters): ")
+def encData(sentence, key):
     if len(key) <16:
         key += " "*(16-len(key))
+    
     key = key[:16]
-                         
-    print "UserKey: %s" % key 
+    
     s = generateKey(key)
-
-    sentence =raw_input("Enter Sentence(0-16 characters): ")
+    
     if len(sentence) <16:
         sentence += " "*(16-len(sentence))
     sentence = sentence[:16]
     
     orgi,cipher = encrypt(sentence,s)
     esentence = deBlocker(cipher)
+
+    return (orgi, cipher, esentence)
+
+def enc():
+    print "ENCRYPTION: "
+
+    key =raw_input("Enter Key(0-16 characters): ")
+
+                         
+    print "UserKey: %s" % key 
+
+    sentence =raw_input("Enter Sentence(0-16 characters): ")
+
+    orgi, cipher, esentence = encData(sentence, key)
     
     print "\nInput String: %s" % sentence 
     print "Original String list: ",orgi
@@ -41,21 +51,11 @@ def cenc():
         print "Usage: python cenc.py <key> <string>"
         sys.exit(0)
 
-    key = sys.argv[1]
-    if len(key) <16:
-        key += " "*(16-len(key))
-    key = key[:16]
-                         
-    s = generateKey(key)
+    key = sys.argv[1]                   
     sentence = sys.argv[2]
-    if len(sentence) <16:
-        sentence += " "*(16-len(sentence))
-    sentence = sentence[:16]
-    
-    orgi,cipher = encrypt(sentence,s)
-    esentence = deBlocker(cipher)
-    
-    
+
+    orgi, cipher, esentence = encData(sentence, key)
+        
     print "\nEncrypted String list: ",cipher
     print "Encrypted String: %s" % esentence
     f = open("encrypted.txt","w")
