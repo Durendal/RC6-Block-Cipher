4ea277182e88051609fb04f1d8a6e29db054ce42
Durendal <durendal@durendals-domain.com>
2016-10-23 22:36:05 -0600

    Updated __init__.py to include key.py

--- rc6/__init__.py
+++ rc6/__init__.py
@@ -1,4 +1,5 @@
-__all__ = ["ops.encrypt", "ops.decrypt", "helpers"]
+__all__ = ["ops.encrypt", "ops.decrypt", "helpers", "key.Key"]
 
 from ops import encrypt, decrypt
 import helpers
+from key import Key
