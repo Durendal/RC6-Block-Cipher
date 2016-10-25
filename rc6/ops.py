36eda88045f453c3c7bfcd22d1cf5070b54eb654
Durendal <durendal@durendals-domain.com>
2016-10-25 04:29:53 -0600

    reduced encryptBlock and decryptBlock

--- rc6/ops.py
+++ rc6/ops.py
@@ -35,15 +35,11 @@ def encryptBlock(sentence, k):
 	A, B, C, D, w, r, b, modulo, lgw, orgi = setup(sentence)  
 	B = (B + s[0]) % modulo
 	D = (D + s[1]) % modulo 
-	for i in range(1, r + 1):
-		t_temp = (B * (2 * B + 1)) % modulo 
-		t = ROL(t_temp, lgw, b)
-		u_temp = (D * (2 * D + 1)) % modulo
-		u = ROL(u_temp, lgw, b)
-		tmod= t % w
-		umod= u % w
-		A = (ROL(A^t, umod, b) + s[2 * i]) % modulo 
-		C = (ROL(C^u, tmod, b) + s[2 * i + 1]) % modulo
+	for i in range(1, r + 1): 
+		t = ROL((B * (2 * B + 1)) % modulo, lgw, b)
+		u = ROL((D * (2 * D + 1)) % modulo, lgw, b)
+		A = (ROL(A^t, u % w, b) + s[2 * i]) % modulo 
+		C = (ROL(C^u, t % w, b) + s[2 * i + 1]) % modulo
 		(A, B, C, D)  =  (B, C, D, A)
 	A = (A + s[2 * r + 2]) % modulo 
 	C = (C + s[2 * r + 3]) % modulo
@@ -64,14 +60,10 @@ def decryptBlock(esentence,k):
 	for j in range(1, r + 1):
 		i = r + 1 - j
 		(A, B, C, D) = (D, A, B, C)
-		u_temp = (D * (2 * D + 1)) % modulo
-		u = ROL(u_temp, lgw, b)
-		t_temp = (B * (2 * B + 1)) % modulo 
-		t = ROL(t_temp, lgw, b)
-		tmod= t % w
-		umod= u % w
-		C = (ROR((C - s[2 * i + 1]) % modulo, tmod, b) ^ u)  
-		A = (ROR((A - s[2 * i]) % modulo, umod, b) ^ t) 
+		u = ROL((D * (2 * D + 1)) % modulo, lgw, b) 
+		t = ROL((B * (2 * B + 1)) % modulo, lgw, b)
+		C = (ROR((C - s[2 * i + 1]) % modulo, t % w, b) ^ u)  
+		A = (ROR((A - s[2 * i]) % modulo, u % w, b) ^ t) 
 	D = (D - s[1]) % modulo 
 	B = (B - s[0]) % modulo
 
